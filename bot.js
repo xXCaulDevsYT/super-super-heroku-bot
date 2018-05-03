46e0025ef8a3e4592003d19aa44deaee42cf33b1
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-05-03 16:40:30 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,32 +1,83 @@
- const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
+
+//Some other configuration stuff
+const WelcomeChannelName = "w-g";
+const White = "FFFFFF";
+const creator = "xXCaulDevsYT#7314";
+const programmer = "FunnyHackz&ModzYT#3804";
+const prefix = "-";
+const token = "NDQxNDIxNzQ1NzY2MDA2ODA1.DcwF5A.xb-qlGxqORQwFyfD1_nTFFdEAOQ";
+//End of configuration
+
+let jokes = ["**Why did Nazir cross the road?**", "**Nazir is Da Wae**"];
+let jokeoutput = Math.floor(Math.random() * jokes.length);
+
 client.on('ready', () => {
-    console.log('I am ready!');
+	console.log("Username : " + client.user.username);
+	console.log("Prefix   : " + prefix);
 });
 
-client.on('message', message => {
-    if (message.content === '-bothelp') {
-    	message.reply('**BTK Help:** -> `-botprefix` `-botinfo` `-subnazir`! ');
-  	}
-});
+
+client.on("guildMemberAdd", member => {
+	let WelcomeChannel = member.guild.channels.find("name", WelcomeChannelName);
+	let embed = new Discord.RichEmbed()
+	.setTitle("Welcome")
+	.setColor(White)
+	.setDescription(`Welcome ${member.user.username} to the official Discord of the BTK Clan!`)
+	WelcomeChannel.send(embed);
+
+})
 
 client.on('message', message => {
-    if (message.content === '-botprefix') {
-    	message.reply('Prefix For BTK Clan Is: ( - )');
-  	}
-});
+	if(message.channel.type === "dm") return;
+	if(message.author.id === client.user.id) return;
+	
+	if(!message.content.startsWith(prefix)) return;
 
-client.on('message', message => {
-    if (message.content === '-botinfo') {
-    	message.reply('Creator: xXCaulDevsYT#7314 , http://youtube.com/emeraldassasinplayz');
-  	}
-});
+	let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    let args = message.content.split(" ").slice(1);
+    let messageArray = message.content.split(" ");
+	let cmd = messageArray[0];
 
-client.on('message', message => {
-    if (message.content === '-subnazir') {
-    	message.reply('https://www.youtube.com/channel/UCUOjDEjAJAEYLVMpUrMxlIQ');
-  	}
+	if (command === "bothelp") {
+		message.channel.send("**BTK Help:** -> `-botprefix` `-botinfo` `-subnazir`! ");
+	}
+
+    if (command === "botprefix") {
+		message.channel.send(`Prefix For BTK Clan Is: ( ${prefix} )`);
+	}
+
+	if (command === "jokes") {
+		
+		message.channel.send(jokes[jokeoutput]);
+	}
+
+	if (command === "botinfo") {
+		let embed = new Discord.RichEmbed()
+		.setTitle("BotInfo")
+		.setColor("FFFFFF")
+		.addField("Username", client.user.username)
+		.addField("ID", client.user.id)
+		.addField("Users", client.users.size)
+		.addField("Guilds", client.guilds.size)
+		.addField("Developer", creator)
+		.addField("Programmer", programmer)
+	    .addField("Developer YT Link", "http://youtube.com/emeraldassasinplayz");
+		message.channel.send(embed);
+	}
+
+	if(command === "subnazir") {
+		let nazir = "https://www.youtube.com/channel/UCUOjDEjAJAEYLVMpUrMxlIQ"; 
+		let embed = new Discord.RichEmbed()
+		.setTitle("Nazir")
+		.setColor("FFFFFF")
+		.setDescription(`[Click Me](${nazir})`);
+		message.channel.send(embed);
+	}
 });
 
 client.on('message', message => {
@@ -34,11 +85,7 @@ client.on('message', message => {
     	message.reply('To Win A Game Of Rules Of Survival!');
   	}
 });
-client.on('message', message => {
-    if (message.content === '-jokes') {
-    	message.reply('**Why did Nazir cross the road?** ');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
+// client.login(token);
