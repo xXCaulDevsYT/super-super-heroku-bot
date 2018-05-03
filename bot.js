9998c17775b4fde4f5b65e02a504d6cab8fa76a7
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-05-02 21:17:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'info') {
+    	message.reply('btk clan bot made by @xXCaulDevsYT#7314');
   	}
 });
 
