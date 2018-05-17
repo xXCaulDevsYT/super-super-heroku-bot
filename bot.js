27507b3d0da57420b3bf0ca6c126a60ec21b4854
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-05-17 17:53:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '-botinfo') {
+    	message.reply('bot made and developed by Caulfergi / ZarkysMC');
   	}
 });
 
